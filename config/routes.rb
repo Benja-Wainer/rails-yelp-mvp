bdc9723c1fc9425741a61b5a53d5d11ded423bc4
Benjamin Wainer <benjawainer@gmail.com>
2023-07-04 08:20:40 +0900

    User can see all restaurants page, add new restaurants, see specific restaurants' info with reviews, and add his own reviews.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'reviews/new'
+  resources :restaurants, only: %i[index show new create] do
+    resources :reviews, only: [:new, :create]
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
